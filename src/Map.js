f63aea98ee90bd723d81793cc1e33f98d6f06676
Carter Deacon <carter.deacon@gmail.com>
2022-02-08 16:56:40 +1000

    resolving conflicts

---
+++ src/Map.js
@@ -0,0 +1,35 @@
+
+   
+import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Link } from "react-router-dom";
+import { useState } from 'react'
+
+
+class SimpleMap extends React.Component {
+  static defaultProps = {
+    center: {
+      lat: -37.8136,
+      lng: 144.9631
+    },
+    zoom: 11
+  };
+
+  render() {
+    return (
+      // Important! Always set the container height explicitly
+      <div style={{ height: "100vh", width: "100%" }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{
+            key: "AIzaSyAzG_OvQF8hcn_wl3mcg4NH6czinvIIHrM"
+          }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+        </GoogleMapReact>
+      </div>
+    );
+  }
+}
+
+export default SimpleMap;
